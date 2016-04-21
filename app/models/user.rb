f0e2fc691bdc674791c480a1db8471083a6cdcb1
Blickhan <jblickhan11@gmail.com>
2016-04-21 17:28:58 -0500

    finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	#before_save { self.username = username.downcase }
 	VALID_USERNAME_REGEX = /\A[A-Za-z0-9_-]+\z/
-	validates :username, presence: true, length: { minimum: 6, maximum: 20 },
+	validates :username, presence: true, length: { minimum: 3, maximum: 20 },
 												format: { with: VALID_USERNAME_REGEX },
 												uniqueness: { case_sensitive: false }
 	has_secure_password
