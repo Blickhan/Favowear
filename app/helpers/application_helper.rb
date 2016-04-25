eac627a41f40a8d80c040c7412b276ba4da8e72c
Jason Blickhan <jblickhan11@gmail.com>
2016-04-24 17:47:38 -0700

    renamed to Favowear

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Pacific Depths"
+    base_title = "Favowear"
     if page_title.empty?
       base_title
     else
