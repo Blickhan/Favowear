ce0832d05cf9652de0466f8673a46254102a2573
Jason Blickhan <jblickhan11@gmail.com>
2016-05-05 10:18:20 -0700

    users not logged in view all posts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-  	@feed_items = current_user.feed.paginate(page: params[:page])#Post.all.paginate(page: params[:page])
+  	if logged_in?
+  		@feed_items = current_user.feed.paginate(page: params[:page])
+  	else
+  		@feed_items = Post.all.paginate(page: params[:page])
+  	end
   end
 
   def help
