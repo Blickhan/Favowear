5e931796ced7651b1696d655def2150be08f205f
Jason Blickhan <jblickhan11@gmail.com>
2016-05-19 16:59:06 -0700

    back to http helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,9 +3,10 @@ module PostsHelper
 	# Returns the full title on a per-page basis.
   def full_url(my_url = '')
     if !my_url.include?('http')
-    	'https://' + my_url
-    elsif !my_url.include?('https') && my_url.include?('http')
-    	my_url.sub('http','https')
+      'http://' + my_url
+    #	'https://' + my_url
+    #elsif !my_url.include?('https') && my_url.include?('http')
+    #	my_url.sub('http','https')
     else
     	my_url
     end
