0350e2165c35da1be8de5cb0240f86507a8262d1
Jason Blickhan <jblickhan11@gmail.com>
2016-06-03 16:19:39 -0700

    new post error message

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   belongs_to :category
   default_scope -> { highest_score }#order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: { message: "missing. Please assign an existing category." }
   validates :image_link, presence: true, length: { maximum: 2048 }
   validates :buy_link, presence: true, length: { maximum: 2048 }
 
